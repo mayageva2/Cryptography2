61317631a716ce312aea4a74258dc8e68f9c21af
mayageva2 <mayageva7@gmail.com>
2025-12-14 13:00:16 +0200

    ket creation complete

--- RSA_implementation.py
+++ RSA_implementation.py
@@ -2,3 +2,18 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+
+KEY_SIZE = 2048
+
+# This function generates the RSA private key object.
+# It automatically finds two large primes p and q,
+# calculates the modulus n = p*q, and finds the private exponent d.
+private_key = rsa.generate_private_key(
+    public_exponent=65537,  #presentation example for e, efficient and secure
+    key_size=KEY_SIZE
+)
+
+public_key = private_key.public_key()
+print("--- RSA Keys Generated ---")
+print(f"Key Size: {KEY_SIZE} bits (Modulus n)")
+
